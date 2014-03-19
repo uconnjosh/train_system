97fb1a6810cbb40c5a629aab0106f5ad44e29be0
Drew Seminara & Josh Paul <student@epicodus.com>
2014-03-19 14:08:15 -0700

    create object for stop

---
+++ lib/stops.rb
@@ -0,0 +1,37 @@
+class Stops
+  attr_reader :stations_id, :lines_id, :id
+
+  def initialize(stations_id, lines_id, id=nil)
+    @stations_id = stations_id
+    @lines_id = lines_id
+    @id = id
+  end
+
+  def ==(another_stop)
+    self.stations_id == another_stop.stations_id
+    self.lines_id == another_stop.lines_id
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM stops;")
+    all_stops = []
+    results.each do |result|
+      stations_id = result['stations_id'].to_i
+      lines_id = result['lines_id'].to_i
+      id = result['id'].to_i
+      all_stops << Stops.new(stations_id, lines_id)
+    end
+    all_stops
+  end
+
+  def save
+     results = DB.exec("INSERT INTO stops (stations_id, lines_id) VALUES ('#{@stations_id}','#{@lines_id}') RETURNING id;")
+     @id = results.first['id'].to_i
+  end
+
+end
+
+
+# SELECT * FROM stations JOIN stops ON stops.station_id = stations.id JOIN lines ON lines.id = stops.lines_id
+
+# SELECT * FROM stops WHERE stations_id = ('#{stations_id}') RETURNING lines_id
---
+++ spec/stops_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'Stops' do
+  it 'initializes a new instance of stops' do
+    new_stop = Stops.new(1, 1)
+    new_stop.should be_an_instance_of Stops
+  end
+
+  it 'is the same stop if it has the same station-line ids' do
+    new_stop1 = Stops.new(1, 1)
+    new_stop2 = Stops.new(1, 1)
+    new_stop1.should eq new_stop2
+  end
+
+  it 'starts off with no station - line pair' do
+    new_stop = Stops.new(1, 1)
+    Stops.all.should eq []
+  end
+
+  it 'lets you save to the database' do
+    new_stop = Stops.new(1, 1)
+    new_stop.save
+    Stops.all.should eq [new_stop]
+  end
+end
